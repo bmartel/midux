b923a39479b5c6822b463f95f5aa0c2438363bff
Brandon Martel <brandonmartel@gmail.com>
2016-06-27 22:08:35 -0500

    add npmignore and export single file

--- src/midux.js
+++ src/midux.js
@@ -1,7 +1,6 @@
 import m from 'mithril';
-import { bindActionCreators } from 'redux';
-
-export * from './store';
+import * as ud from 'ud';
+import { createStore, applyMiddleware, combineReducers, bindActionCreators } from 'redux';
 
 export const defaultMapStateToProps = (state, props) => state;
 
@@ -80,4 +79,49 @@ export const connectStore = (store) =>
       return m(component, {config, actions, ...props, ...storeProps}, children);
     }
   }
+}
+
+
+/**
+ * Store intializers
+ */
+let module;
+
+export function initModule (m) {
+  module = m;
+}
+
+export function defn (...args) {
+  return ud.defn.apply(ud, [module, ...args]);
+}
+
+export function defonce (...args) {
+  return ud.defonce.apply(ud, [module, ...args]);
+}
+
+export function applyUDMiddleware (...middlewares) {
+  const udMiddlwares = middlewares.map(
+      (m, i) => defn(m, `applyUDMiddleware:${i}`));
+  return applyMiddleware.apply(applyMiddleware, udMiddlwares);
+}
+
+export function configureUDStore ({middleware, reducers, state}) {
+  const createStoreFromMiddleware =
+    defn(middleware, 'createStoreFromMiddleware')(createStore);
+  const udReducers = combineUDReducers(reducers);
+  const store = createStoreFromMiddleware(udReducers, state);
+  return defonce(() => store);
+}
+
+function combineUDReducers (reducers) {
+  const udReducers = {};
+  const keys = Object.keys(reducers);
+  let i = keys.length;
+
+  while(i--) {
+    const name = keys[i];
+    udReducers[name] = defn(reducers[name], `combineUDReducers:${name}`);
+  }
+
+  return combineReducers(udReducers);
 }
