0388ac332607f30c9226459094142c8dff127686
Brandon Martel <brandonmartel@gmail.com>
2017-03-24 21:37:30 -0500

    add redux boilerplate functions

--- src/midux.js
+++ src/midux.js
@@ -1,81 +1,120 @@
 /* eslint-disable */
-import m from 'mithril';
-import prop from 'mithril/stream';
+import m from 'mithril'
+import prop from 'mithril/stream'
 import {
   createStore,
   applyMiddleware,
   combineReducers,
   bindActionCreators
-} from 'redux';
+} from 'redux'
 
-export const defaultMapStateToProps = (state, props) => state;
+/**
+ * Generates a new action creator for dispatching requests.
+ *
+ * @param type
+ * @param argNames
+ * @returns {Function}
+ */
+export const makeActionCreator = (type, ...argNames) => (...args) => {
+  let action = { type }
+  argNames.forEach((arg, index) => {
+    action[argNames[index]] = args[index]
+  })
+  return action
+}
+
+/**
+ *  Generates a new reducer for handling action requests
+ *
+ * @param initialState
+ * @param handlers
+ * @returns {Function}
+ */
+export const createReducer = (initialState, handlers) => (state = initialState, action) => {
+  if (handlers.hasOwnProperty(action.type)) {
+    return handlers[action.type](state, action)
+  } else {
+    return state
+  }
+}
+
+/**
+ * Default prop to store mapping
+ *
+ * @param state
+ * @param props
+ */
+export const defaultMapStateToProps = (state, props) => state
 
 /**
  * Connect container component to redux store
  *
- * @param selector
- * @returns {*}
+ * @param store
  */
 export const connectStore = (store) =>
   (mapStateToProps, mapActionCreators = {}) => (component) => {
   return {
     oninit(vnode) {
-      this.store = store;
-      this.componentState = prop({});
-      this.unsubscribe = null;
-      this.actions = bindActionCreators(mapActionCreators, this.store.dispatch);
+      this.store = store
+      this.componentState = prop({})
+      this.unsubscribe = null
+      this.actions = bindActionCreators(mapActionCreators, this.store.dispatch)
 
-      this.isSubscribed = () => typeof this.unsubscribe === 'function';
+      this.isSubscribed = () => typeof this.unsubscribe === 'function'
 
       this.trySubscribe = () => {
         if (!this.isSubscribed()) {
-          this.unsubscribe = this.store.subscribe(this.handleUpdate.bind(this, vnode));
-          this.handleUpdate(vnode);
+          this.unsubscribe = this.store.subscribe(this.handleUpdate.bind(this, vnode))
+          this.handleUpdate(vnode)
         }
-      };
+      }
 
       this.tryUnsubscribe = () => {
         if (this.isSubscribed()) {
-          this.unsubscribe();
-          this.unsubscribe = null;
+          this.unsubscribe()
+          this.unsubscribe = null
         }
-      };
+      }
 
       this.handleUpdate = (vnode) => {
-        if (!this.isSubscribed()) return true;
-        const ownProps = vnode.attrs || {};
-        const storeState = mapStateToProps(this.store.getState(), ownProps);
+        if (!this.isSubscribed()) return true
+        const ownProps = vnode.attrs || {}
+        const storeState = mapStateToProps(this.store.getState(), ownProps)
 
-        this.componentState(storeState);
-      };
+        this.componentState(storeState)
+      }
 
-      this.trySubscribe();
+      this.trySubscribe()
     },
 
     onremove(vnode) {
-      this.actions = null;
-      this.store = null;
-      this.componentState = null;
-      this.tryUnsubscribe();
+      this.actions = null
+      this.store = null
+      this.componentState = null
+      this.tryUnsubscribe()
     },
 
     view (vnode, children) {
-      const actions = this.actions;
-      const storeProps = this.componentState();
+      const actions = this.actions
+      const storeProps = this.componentState()
 
-      return m(component, { actions, ...storeProps, ...vnode.attrs}, children);
+      return m(component, { actions, ...storeProps, ...vnode.attrs}, children)
     }
   }
 }
 
 /**
-* Configure store to use reducers/middleware
-*/
+ * Configure store to use reducers/middleware
+ *
+ * @param reducers
+ * @param middleware
+ * @returns {Store<S>}
+ */
 export const configureStore = (reducers, middleware = []) => {
   /**
    * Build app state defined by data reducers shape
    */
-  const initialState = window.__INITIAL_STATE__ || {};
+  const initialState = window.__INITIAL_STATE__ || {}
 
   /**
    * Create data store from the defined data shape
@@ -84,5 +123,5 @@ export const configureStore = (reducers, middleware = []) => {
     combineReducers(reducers),
     initialState,
     applyMiddleware(...middleware)
-  );
+  )
 }
